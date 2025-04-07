75bbe64fb86db8693213e55999baffaaf3240852
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2025-04-07 12:54:35 +0800

    adaptations to run with our development test environment
    Some of these include providing deterministic order of the test
    object and node restrictions as well as re-enabling old TODOs
    of older Cartesian parser bugs.
    
    TODO: We don't have a print_dicts alternative for now.

--- avocado_i2n/params_parser.py
+++ avocado_i2n/params_parser.py
@@ -31,7 +31,7 @@ import copy
 import collections
 import logging
 
-from virttest import cartesian_config
+from cartconf.parser import Parser
 from virttest.utils_params import Params
 from avocado.core.settings import settings
 
@@ -335,7 +335,7 @@ class Reparsable:
         show_dict_fullname: bool = False,
         show_dict_contents: bool = False,
         show_empty_cartesian_product: bool = True,
-    ) -> cartesian_config.Parser:
+    ) -> Parser:
         """
         Get a basic parameters parser with its dictionaries.
 
@@ -348,7 +348,7 @@ class Reparsable:
         :returns: resulting parser
         :raises: :py:class:`EmptyCartesianProduct` if no combination of the restrictions exists
         """
-        parser = cartesian_config.Parser()
+        parser = Parser()
         hostname = os.environ.get("PREFIX", os.environ.get("HOSTNAME", "avocado"))
         parser.parse_string("hostname = %s\n" % hostname)
         suite_path = settings.as_dict().get("i2n.common.suite_path")
@@ -382,18 +382,19 @@ class Reparsable:
                 try:
                     peek_dict = peek_generator.__next__()
                     if show_dictionaries:
-                        cartesian_config.print_dicts(
+                        # TODO: this requires access to the parse python script
+                        print_dicts(
                             options(False, show_dict_fullname, show_dict_contents),
                             (peek_dict,),
                         )
-                        cartesian_config.print_dicts(
+                        print_dicts(
                             options(False, show_dict_fullname, show_dict_contents),
                             peek_generator,
                         )
                 except StopIteration:
                     raise EmptyCartesianProduct(str(self)) from None
             else:
-                cartesian_config.print_dicts(
+                print_dicts(
                     options(False, show_dict_fullname, show_dict_contents),
                     peek_generator,
                 )
