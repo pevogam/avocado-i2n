01a92f6b9f3ff3133ada356adeecb8e2de4211d9
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2025-04-12 20:54:09 +0800

    recover better default get policy
    The pre-installation terminal node will now use default VT parameters
    to always create a blank initial image for the installation test
    to follow using the default environment preprocessing.
    
    TODO: Review or squash.

--- avocado_i2n/cartgraph/graph.py
+++ avocado_i2n/cartgraph/graph.py
@@ -1930,10 +1930,11 @@ class TestGraph(object):
         setup_dict.update(
             {
                 "type": "shared_configure_install",
+                "skip_image_processing": "no",
+                "create_image": "yes",
+                "force_create_image": "yes",
                 # overwrite some params inherited from the modified install node
                 f"set_state_images_{object_image}_{object_vm}": "",
-                "show_mode": "rf",  # implicit root handling
-                "start_vm": "no",
             }
         )
         pre_node = TestGraph.parse_node_from_object(
